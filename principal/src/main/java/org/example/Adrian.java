5334b8d4ba122be0ee7a9c8937cdc9657bf5e686
Adrianreg <adrian.regalado.bntz@gmail.com>
2022-03-29 19:41:07 +0100

    Proyeco finalizado

---
+++ principal/src/main/java/org/example/Adrian.java
@@ -0,0 +1,49 @@
+public class Adrian {
+
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+
+    /**
+     * Constructor por defecto
+     */
+    public Adrian();
+
+    /**
+     * Constructor con 1 parametro
+     * @param nombre del alumno
+     */
+    public Adrian(String nombre){
+        this.nombre = nombre;
+    }
+
+    /**
+     * Constructor con 2 parametros
+     * @param nombre del alumno
+     * @param apellido1 del alumno
+     */
+    public Adrian(String nombre, String apellido1){
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+    }
+
+    /**
+     * Constructor con 3 parametros
+     * @param nombre del alumno
+     * @param apellido1 del alumno
+     * @param apellido2 del alumno
+     */
+    public Adrian(String nombre, String apellido1, String apellido2){
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+
+    /**
+     * Método que realiza la validación de las propiedades de la clase.
+     */
+    public void String validarNombre{
+
+    }
+
+}
